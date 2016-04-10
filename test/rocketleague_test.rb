cdeea16b65d1dfbbc1043a4621b17bf3b3e33362
jomo <github@jomo.tv>
2016-04-10 05:56:40 +0200

    add tests

--- test/rocketleague_test.rb
+++ test/rocketleague_test.rb
@@ -1,7 +1,61 @@
 require 'test_helper'
 
-class RocketLeagueTest < Minitest::Test
-  def test_that_it_has_a_version_number
+describe RocketLeague do
+  before do
+    @rl = RocketLeague::API.new "https://example.com", 1, "Steam", "loginkey", "callprockey"
+  end
+
+  it "must have a version number" do
     refute_nil ::RocketLeague::VERSION
+  end
+
+  it "must encode proc calls correcly" do
+    encoded = @rl.procencode([["ProcWithNoArgs"], ["ProcWithArgs", "123456789", "abcdefg"]])
+    expected = "&P1P[]=123456789&P1P[]=abcdefg&Proc[]=ProcWithNoArgs&Proc[]=ProcWithArgs"
+    assert_equal expected, encoded
+  end
+
+  it "must parse proc responses correcly" do
+    # features leading empty line, multiple intermediate empty lines, trailing empty line, duplicate results
+    response = <<-END.gsub(/^    /, '')
+
+    DataKey=Analytics&DataValue=1
+    DataKey=BugReports&DataValue=0
+    DataKey=RankEnabled&DataValue=1
+
+
+
+    DataKey=Analytics&DataValue=1
+    DataKey=BugReports&DataValue=0
+    DataKey=RankEnabled&DataValue=1
+
+    DataKey=Analytics&DataValue=1
+    DataKey=BugReports&DataValue=0
+    DataKey=RankEnabled&DataValue=1
+
+    END
+    parsed = @rl.procparse(response)
+    expected = [
+      [],
+      [
+        {"DataKey"=>"Analytics", "DataValue"=>"1"},
+        {"DataKey"=>"BugReports", "DataValue"=>"0"},
+        {"DataKey"=>"RankEnabled", "DataValue"=>"1"}
+      ],
+      [],
+      [],
+      [
+        {"DataKey"=>"Analytics", "DataValue"=>"1"},
+        {"DataKey"=>"BugReports", "DataValue"=>"0"},
+        {"DataKey"=>"RankEnabled", "DataValue"=>"1"}
+      ],
+      [
+        {"DataKey"=>"Analytics", "DataValue"=>"1"},
+        {"DataKey"=>"BugReports", "DataValue"=>"0"},
+        {"DataKey"=>"RankEnabled", "DataValue"=>"1"}
+      ],
+      []
+    ]
+    assert_equal expected, parsed
   end
 end
